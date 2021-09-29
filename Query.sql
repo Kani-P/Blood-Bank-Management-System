5080c1650019c73d36634f4943fe68b4a4592c61
Kani-P <88034940+Kani-P@users.noreply.github.com>
2021-09-29 16:51:02 +0530

    Add files via upload

---
+++ Query.sql
@@ -0,0 +1,8 @@
+create table donor(id int(10),name varchar(50),father varchar(50),mother varchar(50),mobilenumber varchar(10),
+gender varchar(10),email varchar(50),bloodgroup varchar(10),address varchar(200));
+
+insert into donor(id,name,father,mother,mobilenumber,gender,email,bloodgroup,address)
+values(1,'jishak','unnikrishnan','geetha','9876543211','female','jisha123@gmail.com','O+','Kalladi,Kerala,India');
+
+insert into donor(id,name,father,mother,mobilenumber,gender,email,bloodgroup,address)
+values(2,'Pavithra','Ram','Janu','9876543211','female','pavithra123@gmail.com','O+','Salem,Tamil Nadu,India');
